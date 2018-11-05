b56ca8ce1c9c6afb9f2f914fe350a129f3495dce
Alex Wilson <fonzmp@gmail.com>
2018-11-05 16:31:32 -0500

    set signup to work as modal and report errors

--- web/src/components/Signup.js
+++ web/src/components/Signup.js
@@ -1,27 +1,109 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { userServices } from "../_services/userServices";
 
 class Signup extends Component {
-    render() {
-        return (
-            <div className="container">
-                <h1>Signup</h1>
-                <p>Please fill out the information below: </p>
-                <form>
-                    <label>Name: </label>
-                    <input type="text" placeholder="name"></input>
-                    <label>Email: </label>
-                    <input type="email" placeholder="email"></input>
-                    <label>Phone: </label>
-                    <input type="tel" placeholder="phone"></input>
-                    <label>Username: </label>
-                    <input type="text" placeholder="username"></input>
-                    <label>Password: </label>
-                    <input type="password" placeholder="password"></input>
-                    <button>Submit</button>
-                </form>
-            </div>
-        )
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      telephone: "",
+      username: "",
+      password: ""
+    };
+  }
+
+  componentWillMount() {
+    document.addEventListener("mousedown", this.handleClick, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClick, false);
+  }
+
+  handleClick = e => {
+    if (this.node.contains(e.target)) {
+      return;
     }
+
+    this.closeSignup();
+  };
+
+  closeSignup = () => {
+    this.props.closeSignup();
+  };
+
+  formSubmit = e => {
+    e.preventDefault();
+    this.props.dispatch(userServices.signupUser(this.state));
+  };
+
+  handleOnChange = e => {
+    this.setState({
+      [e.target.id]:
+        e.target.type === "Number" ? parseInt(e.target.value) : e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="modal-backing">
+        <div className="modal" ref={node => (this.node = node)}>
+          <h1>Signup</h1>
+          <p>Please fill out the information below: </p>
+          <form onSubmit={this.formSubmit}>
+            <label>Name: </label>
+            <input
+              id="name"
+              type="text"
+              onChange={this.handleOnChange}
+              value={this.state.name}
+              placeholder="name"
+            />
+            <label>Email: </label>
+            <input
+              id="email"
+              type="email"
+              onChange={this.handleOnChange}
+              value={this.state.email}
+              placeholder="email"
+            />
+            <label>Phone: </label>
+            <input
+              id="telephone"
+              type="tel"
+              onChange={this.handleOnChange}
+              value={this.state.phone}
+              placeholder="phone"
+            />
+            <label>Username: </label>
+            <input
+              id="username"
+              type="text"
+              onChange={this.handleOnChange}
+              value={this.state.username}
+              placeholder="username"
+            />
+            <label>Password: </label>
+            <input
+              id="password"
+              type="password"
+              onChange={this.handleOnChange}
+              value={this.state.password}
+              placeholder="password"
+            />
+            <button>Submit</button>
+            <button onClick={this.closeSignup}>Close</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Signup;
+function mapStateToProps(state) {
+  return {};
+}
+
+export default connect(mapStateToProps)(Signup);

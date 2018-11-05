ec789f107856d48c73cc2888b4cbe7175abe6938
Alex Wilson <fonzmp@gmail.com>
2018-11-05 00:31:19 -0500

    set up routes and import navbar

--- web/src/index.js
+++ web/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-import Home from './App'
+
+import Signup from './components/Signup';
+import Navbar from './components/Navigation/Navbar';
 
 ReactDOM.render(
     <Router>
         <div>
-            <Route component={App} exact path="/" />
+            <Navbar />
+            <div>
+                <Route component={App} exact path="/" />
+                <Route component={Signup} exact path="/signup" />
+            </div>
         </div>
     </Router>
     , document.getElementById('root'));

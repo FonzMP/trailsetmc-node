159a8b2f5e33d38ac5a18ec254aa5fb936fdbc84
Alex Wilson <fonzmp@gmail.com>
2018-11-04 22:11:10 -0500

    set up Router for index

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          
-        </header>
+      <div>
+        <h1>Home</h1>
       </div>
     );
   }
--- web/src/index.js
+++ web/src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route component={App} exact path="/" />
+        </div>
+    </Router>
+    , document.getElementById('root'));

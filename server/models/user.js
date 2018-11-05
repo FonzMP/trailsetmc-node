b7b6769f0340231d54cb8ce222365dc79ef50dfa
Alex Wilson <fonzmp@gmail.com>
2018-11-05 16:30:47 -0500

    modified contact type within user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Contact = import("./contact");
-
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   telephone: { type: Number, required: true },
   password: { type: String, required: true },
   emergency: {
-    type: Contact
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Contact"
   },
-  contacts: [{ type: Contact }],
+  contacts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Contact"
+    }
+  ],
   dateJoined: {
     type: Date,
     default: Date.now()

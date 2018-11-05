1e119dd89f3ab88edb9e2ebe808c53a641773803
Alex Wilson <fonzmp@gmail.com>
2018-11-05 01:53:32 -0500

    app connected to mLab db

--- server/app.js
+++ server/app.js
@@ -1,10 +1,27 @@
-var express = require('express')
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+const mLab = `mongodb://${process.env.DBUSER}:${
+  process.env.DBPASSWORD
+}@ds151863.mlab.com:51863/trailsetmcnode`;
+
+mongoose.connect(
+  mLab,
+  { useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  console.log("connected to server");
+});
+
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
+
 app.listen(3001, () => {
-    console.log('server has started')
-})
+  console.log("server has started");
+});

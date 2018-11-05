be013cce0803893d146ca53a34996e19f0ebdb52
Alex Wilson <fonzmp@gmail.com>
2018-11-05 16:32:13 -0500

    using app to handle state, relocated router and others from index.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,57 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-import './App.css';
+import "./App.css";
+
+import Navbar from "./components/Navigation/Navbar";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import ErrorModal from "./components/Modals/errorModal";
+
+import { errorServices } from "./_services/errorServices";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displaySignup: false
+    };
+  }
+  clearError = () => {
+    this.props.dispatch(errorServices.clearError());
+  };
+
+  displaySignup = () => {
+    this.setState({
+      displaySignup: !this.state.displaySignup
+    });
+  };
   render() {
+    console.log(this.props);
     return (
-      <div className="container">
-        <h1>Trailset MC</h1>
-        <p>Welcome to Trailset MC, an application focused on providing safety and user peace of mind as a main priority for all riders in the wild. Whether it be a small group ride, large group ride, or solo rider, you all deserve the peace of mind knowing that you'll be safe in what you do.</p>
-      </div>
+      <Router>
+        <div>
+          {this.props.error !== "" ? (
+            <ErrorModal error={this.props.error} closeModal={this.clearError} />
+          ) : null}
+          {this.state.displaySignup ? (
+            <Signup closeSignup={this.displaySignup} />
+          ) : null}
+          <Navbar closeSignup={this.displaySignup} />
+          <div>
+            <Route component={Home} exact path="/" />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    error: state.error.error
+  };
+}
+
+export default connect(mapStateToProps)(App);

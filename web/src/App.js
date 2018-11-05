7d9405c205d1872aaa3c1f11158c1cbefc3f5201
Alex Wilson <fonzmp@gmail.com>
2018-11-05 00:31:01 -0500

    inserted basic text for home page

--- web/src/App.js
+++ web/src/App.js
@@ -5,8 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
+      <div className="container">
+        <h1>Trailset MC</h1>
+        <p>Welcome to Trailset MC, an application focused on providing safety and user peace of mind as a main priority for all riders in the wild. Whether it be a small group ride, large group ride, or solo rider, you all deserve the peace of mind knowing that you'll be safe in what you do.</p>
       </div>
     );
   }

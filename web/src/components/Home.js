3ee4433d8e7342802329ccbd626ed32e8bce6cef
Alex Wilson <fonzmp@gmail.com>
2018-11-05 16:31:18 -0500

    created home to use app for state management

---
+++ web/src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Trailset MC</h1>
+        <p>
+          Welcome to Trailset MC, an application focused on providing safety and
+          user peace of mind as a main priority for all riders in the wild.
+          Whether it be a small group ride, large group ride, or solo rider, you
+          all deserve the peace of mind knowing that you'll be safe in what you
+          do.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;

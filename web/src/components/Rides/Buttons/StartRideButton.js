807d4bfe08b83ccf592b8fb8da4f4161bce47280
Alex Wilson <fonzmp@gmail.com>
2018-11-11 17:20:44 -0500

    created interval to track time on start / stop

--- web/src/components/Rides/Buttons/StartRideButton.js
+++ web/src/components/Rides/Buttons/StartRideButton.js
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
 
 class StartRideButton extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentTime: 0,
+      active: false
+    };
+  }
+
+  startTimer = e => {
+    e.preventDefault();
+    this.setState({ active: true });
+    this.intervalId = setInterval(this.timer, 1000);
+  };
+
+  stopTimer = e => {
+    e.preventDefault();
+    this.setState({ currentTime: this.state.currentTime, active: false });
+    clearInterval(this.intervalId);
+  };
+
+  timer = () => {
+    this.setState({
+      currentTime: this.state.currentTime + 1
+    });
+  };
+
   render() {
-    return <button>Start Ride</button>;
+    console.log(this.state);
+    return (
+      <div>
+        {this.state.active ? (
+          <button onClick={this.stopTimer}>Stop Ride</button>
+        ) : (
+          <button onClick={this.startTimer}>Start Ride</button>
+        )}
+      </div>
+    );
   }
 }
 
